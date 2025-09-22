b2dce0a2b2312234a9dd20db553f228f98ea4858
AlanT011 <krknksungswang@gmail.com>
2025-09-22 15:32:12 +0700

    Delete main.java

--- main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("SN12345", "Toyota", "Camry", "2020", 24000.0f);
-        System.out.println("Vehicle Created: " + vehicle);
-    }
-}
